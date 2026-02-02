18805e9a812351165ea856f8e82274ddc2d84639
Aleksandar Tesinic <aleksandartesinic820@gmail.com>
2026-02-02 21:26:27 +0100

    test

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+// 👉 Statische Dateien erlauben (HTML, CSS, JS)
+app.use(express.static(__dirname));
+
+app.listen(port, () => {
+  console.log(`Server läuft auf http://localhost:${port}`);
+});
